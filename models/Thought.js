a24168dbe4b56c8cd9dd3fef437d04342136cbdf
Joseph Mogavero <82268976+joemogy@users.noreply.github.com>
2023-02-11 12:36:28 -0500

    Update Thought.js

---
+++ models/Thought.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 280
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now, /// Use a getter method to format the timestamp on query
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
